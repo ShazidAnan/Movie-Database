0fe3bb381a22badbe00fcfbc4f0d9488a2996466
ShazidAnan <shazidanan.bu@gmail.com>
2025-03-20 04:32:56 +0000

    Router

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,6 +2,20 @@
 // Initialize express router 
 import express from 'express' 
 const router = express.Router() 
+const app = express() 
+app.use(express.json()); 
+
+import bodyParser from "body-parser" 
+app.use(bodyParser.urlencoded({ 
+extended: true 
+})); 
+app.use(bodyParser.json()) 
+
+import apiRoutes from "./routes/api-routes.js" 
+
+app.use('/api', apiRoutes); 
+
+const PORT = process.env.PORT || 3000
 // Set default API response 
 router.get('/', function (req, res) { 
 res.json({ 
